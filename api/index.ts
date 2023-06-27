2a9456e837f167ffd5e1aa298483e46313f47bdd
SamuelBrunoo <samuel.k.bruno@gmail.com>
2023-06-27 19:07:57 -0300

    codegarden tutorial

--- api/index.ts
+++ api/index.ts
@@ -1,25 +1,3 @@
-import express, { Request, Response, ErrorRequestHandler } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import apiRoutes from './routes/api';
+import app from "../src"
 
-dotenv.config();
-
-const server = express();
-
-server.use(cors({ origin: '*' }));
-
-server.use(express.urlencoded({ extended: true }));
-
-server.use('/', apiRoutes);
-
-server.use((req: Request, res: Response) => {
-  res.status(404).json({ error: 'Something goes wrong. Please contact our support.' });
-});
-
-const errorHandler: ErrorRequestHandler = (err, req, res) => {
-  res.status(400).json({ error: 'Something goes wrong. Please contact our support.' });
-}
-server.use(errorHandler);
-
-server.listen(process.env.PORT, () => { console.log(`Server running on port ${process.env.PORT}`) });
+export default app
